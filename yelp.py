44fdf644e98a46c5b992ee93876bf2197b8f709e
Timothy Lung <tlung@wustl.edu>
2019-10-05 15:47:35 -0500

    Done
    With radius limiter. Sorry for poor code. Feeling cute, might clean later, idk.

--- yelp.py
+++ yelp.py
@@ -5,79 +5,98 @@ import csv
 
 api_key = 'x7-Y6Xx3bYocGlXvipR-fXVgVDpxzuAEyLQN5ufLJdq5LiLxxP12YwYMMy7EUL3y91SPrGQIGRkUdVgnd_ZaOxLw3fb4WD4sPocqBTtaGDXNYWxIzMKHfzXkkP2XXXYx'
 headers = {'Authorization': 'Bearer %s' % api_key}
+url = 'https://api.yelp.com/v3/businesses/search'
+params = {'term':'restaurants','location':'San Francisco','radius':1000}
+hour_array = [0, False, False, False, False, 1, False, False, False, False, 2, False, False, False, False, 3, False, False, False, False,
+              4, False, False, False, False, 5, False, False, False, False, 6, False, False, False, False, ]
+switch_day = {
+    0: [0, False, False, False, False],
+    1: [0, False, False, False, False],
+    2: [0, False, False, False, False],
+    3: [0, False, False, False, False],
+    4: [0, False, False, False, False],
+    5: [0, False, False, False, False],
+    6: [0, False, False, False, False],
+}
 
-url = 'https://api.yelp.com/v3/businesses/bZ7lvi5_BQ6UKK10OKPV7Q'
-params = {'term':'restaurants','location':'San Francisco'}
+# parse the hours
+def hour_gathering(hours):
+    for hour in hours:
+        print(hour)
+        day = switch_day[hour["day"]]
 
-#req = requests.get(url, params=params, headers=headers) # for search
-req = requests.get(url, headers=headers) # for details
+        start = int(hour["start"])
+        end = int(hour["end"])
 
+        # Get total time open
+        if start == end:
+            day[0] = 24
+            day[1] = True
+            day[2] = True
+            day[3] = True
+            day[4] = True
+        elif end > start:
+            day[0] += end - start
+        else: # If open past midnight
+            day[0] += 24 - end + start
+            day[4] = True
+
+        # Overnight & Morning/Afternoon/Evening
+        if hour["is_overnight"]:
+            day[4] = True
+        if start > end or start < 11:
+            day[1] = True
+        if end > 14 and start < 12:
+            day[2] = True
+        if end > 19 and start < 19:
+            day[3] = True
+
+    for key, value in switch_day.items():
+        key_index = key * 5
+        for i in range(5):
+            hour_array[key_index+i] = value[i]
+    for thing in hour_array:
+        row.append(thing)
+    return row
+
+
+req = requests.get(url, params=params, headers=headers) # for search
 parsed = json.loads(req.text)
-
-#businesses = parsed["businesses"]
-
-print(parsed["name"])
-print(parsed["coordinates"]["latitude"])
-print(parsed["coordinates"]["longitude"])
-print(parsed["hours"])
-print(parsed["is_closed"])
-print(parsed["price"])
-print(parsed["rating"])
-print(parsed["review_count"])
-print(parsed["transactions"])
-#print(parsed["attributes"])
+businesses = parsed["businesses"]
 
 with open('test.csv', mode='w') as employee_file:
+    #CSV Setup
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
     employee_writer.writerow(
-        ["Name", "Latitude", "Longitude", "Closed", "Price", "Rating", "Review Count", "Transactions", "Attributes",
-         "Monday Overnight", "Monday Op"])
-    try:
-        employee_writer.writerow([parsed["name"], parsed["coordinates"]["latitude"], parsed["coordinates"]["longitude"],
-                                  parsed["is_closed"], parsed["price"], parsed["rating"], parsed["review_count"],
-                                  parsed["transactions"], parsed["attributes"]])
-    except:
-        employee_writer.writerow([parsed["name"], parsed["coordinates"]["latitude"], parsed["coordinates"]["longitude"],
-                                  parsed["is_closed"], parsed["price"], parsed["rating"], parsed["review_count"],
-                                  parsed["transactions"], "", ])
+        ["Name", "Latitude", "Longitude", "Closed", "Rating", "Review Count", "Transactions", "Price", "Attributes",
+         "Day 0 Hours", "Day 0 Morning", "Day 0 Afternoon", "Day 0 Evening", "Day 0 Overnight",
+         "Day 1 Hours", "Day 1 Morning", "Day 1 Afternoon", "Day 1 Evening", "Day 1 Overnight",
+         "Day 2 Hours", "Day 2 Morning", "Day 2 Afternoon", "Day 2 Evening", "Day 2 Overnight",
+         "Day 3 Hours", "Day 3 Morning", "Day 3 Afternoon", "Day 3 Evening", "Day 3 Overnight",
+         "Day 4 Hours", "Day 4 Morning", "Day 4 Afternoon", "Day 4 Evening", "Day 4 Overnight",
+         "Day 5 Hours", "Day 5 Morning", "Day 5 Afternoon", "Day 5 Evening", "Day 5 Overnight",
+         "Day 6 Hours", "Day 6 Morning", "Day 6 Afternoon", "Day 6 Evening", "Day 6 Overnight"])
 
+    for bus in businesses:
+        url = 'https://api.yelp.com/v3/businesses/' + bus["id"]
 
+        req = requests.get(url, headers=headers)  # for details
+        business = json.loads(req.text)
+        #print(business)
 
+        row = [business["name"], business["coordinates"]["latitude"], business["coordinates"]["longitude"],
+                business["is_closed"], business["rating"], business["review_count"],
+                business["transactions"]]
+        if "price" in business:
+            row.append(len(business["price"]))
+        else:
+            row.append("")
+        if "attributes" in business:
+            row.append(business["attributes"])
+        else:
+            row.append("")
 
-# with open('test.txt') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             print(f'Column names are {", ".join(row)}')
-#             line_count += 1
-#         else:
-#             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#             line_count += 1
-#     print(f'Processed {line_count} lines.')
-
-#Name of business, lat, lon, opening hours of business, overnight hours, permanently closed, price, rating, review count, transactions, attributes
-
-
-# for business in businesses:
-#     print("Name:", business["name"])
-#     print("Rating:", business["rating"])
-#     print("Address:", " ".join(business["location"]["display_address"]))
-#     print("Phone:", business["phone"])
-#     print("\n")
-#
-#     id = business["id"]
-#
-#     url="https://api.yelp.com/v3/businesses/" + id + "/reviews"
-#
-#     req = requests.get(url, headers=headers)
-#
-#     parsed = json.loads(req.text)
-#
-#     reviews = parsed["reviews"]
-#
-#     print("--- Reviews ---")
-#
-#     for review in reviews:
-#         print("User:", review["user"]["name"], "Rating:", review["rating"], "Review:", review["text"], "\n")
+        hours = business["hours"][0]["open"]
+        #print(hours)
+        row = hour_gathering(hours)
+        employee_writer.writerow(row)
